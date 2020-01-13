b881340eba728c0ee89dc225053cd82da9dc5037
Cereal Aris <22aris.cyril@waipahuh.k12.hi.us>
2020-01-12 19:13:58 -1000

    first commit

---
+++ function.js
@@ -0,0 +1,18 @@
+function startTime() {
+  var time = new Date();
+  var h = time.getHours();
+  var m = time.getMinutes();
+  var s = time.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
+}
+function pressButton() {
+  
+}
