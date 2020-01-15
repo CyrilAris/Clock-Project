ae8900f7df1d0d83dc9c37b2cc8ad77c12e3a37a
richard <22aris.cyril@waipahuh.k12.hi.us>
2020-01-15 08:54:28 -1000

    second commit

--- function.js
+++ function.js
@@ -3,16 +3,33 @@ function startTime() {
   var h = time.getHours();
   var m = time.getMinutes();
   var s = time.getSeconds();
+  var ampm = "PM";
   m = checkTime(m);
   s = checkTime(s);
+  if (h < 12) {
+    ampm = "AM"
+  }
+  if (h > 12) {
+    h -=12
+  }
+  if (h < 10) {
+    h = "0" + h
+  }
+  if (m < 10) {
+    m = "0" + m
+  }
+  if (s < 10) {
+    s = "0" + s
+  }
   document.getElementById('time').innerHTML =
-  h + ":" + m + ":" + s;
+  h + ":" + m + ":" + s + " " + ampm;
   var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
   if (i < 10) {i = "0" + i};
   return i;
 }
-function pressButton() {
-  
+function play() {
+var audio = new Audio('https://www.youtube.com/watch?v=moSFlvxnbgk'[original].mp3);
+audio.play();
 }
